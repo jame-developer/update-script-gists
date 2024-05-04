96aad5b152ee70e8ef965fb01f5ca74741c3a983
Jan <github@vedemaj.eu>
2024-05-04 21:51:57 +0200

    fix: explicitly set exit value

--- update_install_golang_gist.py
+++ update_install_golang_gist.py
@@ -5,11 +5,13 @@ import re
 
 
 def get_current_used_go_version():
+    token = os.getenv('GITHUB_TOKEN')
+    gist_id = os.getenv('INSTALL_GOLANG_GIST_ID')
     headers = {
-        'Authorization': f'token {os.getenv('GITHUB_TOKEN')}',
+        'Authorization': f'token {token}',
         'Accept': 'application/vnd.github.v3+json'
     }
-    gist_url = f"https://api.github.com/gists/{os.getenv('INSTALL_GOLANG_GIST_ID')}"
+    gist_url = f"https://api.github.com/gists/{gist_id}"
     response = requests.get(gist_url, headers=headers)
     if response.status_code != 200:
         print(f"Failed to get gist: {response.text}")
@@ -33,8 +35,10 @@ def get_latest_go_version():
 
 
 def update_gist():
+    token = os.getenv('GITHUB_TOKEN')
+    gist_id = os.getenv('INSTALL_GOLANG_GIST_ID')
     headers = {
-        'Authorization': f'token {os.getenv('GITHUB_TOKEN')}',
+        'Authorization': f'token {token}',
         'Accept': 'application/vnd.github.v3+json'
     }    
         
@@ -43,7 +47,7 @@ def update_gist():
     with open('latest_go_version.txt', 'r') as file:
         latest_go_version = file.read().strip()
     
-    gist_url = f"https://api.github.com/gists/{os.getenv('INSTALL_GOLANG_GIST_ID')}"
+    gist_url = f"https://api.github.com/gists/{gist_id}"
     new_content = response.json()['files']['install_golang.sh']['content'].replace(current_go_version,
                                                                                        latest_go_version)
     data = {
@@ -56,6 +60,7 @@ def update_gist():
     response = requests.patch(gist_url, headers=headers, json=data)
     if response.status_code == 200:
         print("Gist updated successfully")
+        return True
     else:    
         print(f"Failed to update gist: {response.text} ")
         return False
@@ -73,10 +78,12 @@ def extract_go_version(input_string):
 if __name__ == "__main__":
     GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
     GIST_ID = os.getenv('INSTALL_GOLANG_GIST_ID')
-
+    success = False
     if sys.argv[1] == 'get_current_version':
-        print(get_current_used_go_version())
+        success = get_current_used_go_version()
     elif sys.argv[1] == 'get_latest_version':
-        print(get_latest_go_version())
+        success = get_latest_go_version()
     elif sys.argv[1] == 'update_gist':
-        update_gist()
+       success = update_gist()        
+    if not success:
+        exit(1)
